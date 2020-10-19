ec8e04f394f4e966c1832e275a2ca35da72f748c
pglazov <pglazov@mirantis.com>
2020-10-19 21:41:28 +0000

    Update cp_script for use autopush

--- cp_script.sh
+++ cp_script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+cd ..
 cp tcp-qa/checklist.yaml ./maintenance_custom_analyst/
 find . -name *skip.list | grep released-heat-cicd >>temp.txt
 NAME=''
+NOCP='true'
 while read LINE
     do 
 	    if [[ $LINE == *"queens"* ]];
@@ -12,12 +14,21 @@ while read LINE
 	    fi
 	    if [[ $LINE == *"contrail"* ]];
 	    then NAME=${NAME}"-contrail"
-	    else NAME=${NAME}"-sl"
+	    else NAME=${NAME}"-dvr"
             fi
-	    echo ${NAME}
-	    cp $LINE ./maintenance_custom_analyst/${NAME}-skip.list
+	    DIFF=$(diff $LINE ./maintenance_custom_analyst/${NAME}-skip.list)
+	    if [[ "$DIFF" != "" ]]
+		 then cp $LINE ./maintenance_custom_analyst/${NAME}-skip.list
+	    		 echo 'copied '${NAME}
+			 	NOPC='false'
+		 else echo 'no copied '${NAME}
+	    fi
 	    NAME=''
 done < temp.txt
 rm temp.txt
-
-
+cd maintenance_custom_analyst
+if [ NOCP == 'false' ];
+then git add --all
+	git commit -m 'New changes in tcp-qa skip lists or checklist'
+	git push
+fi
