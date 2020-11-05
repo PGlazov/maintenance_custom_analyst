52f6a8b95f3fe5a822c1743bf36b9c1f85fcb917
pglazov <pglazov@mirantis.com>
2020-11-05 13:40:21 +0000

    New changes in tcp-qa skip lists or checklist

--- cp_script.sh
+++ cp_script.sh
@@ -3,20 +3,10 @@
 cp tcp-qa/checklist.yaml maintenance_custom_analyst/
 find . -name *skip.list | grep released-heat-cicd >>temp.txt
 NAME=''
-NOCP='true'
+NOCP='false'
 while read LINE
     do 	    NAME=$LINE
 	    NAME=$(echo $NAME | sed 's/.\/tcp-qa\/tcp_tests\/templates\/released-heat-cicd-//' | sed 's/\/tempest_skip.list//')
-	    #if [[ ${LINE} == *"queens"* ]];
-	    #then NAME=${NAME}"queens"
-	    #fi
-            #if [[ ${LINE} == *"pike"* ]];            
-            #then NAME=${NAME}"pike"
-	    #fi
-	    #if [[ ${LINE} == *"contrail"* ]];
-	    #then NAME=${NAME}"-contrail-sl"
-	    #else NAME=${NAME}"-dvr-sl"
-            #fi
 	    echo $NAME
 	    echo $LINE
 	    DIFF=$(diff $LINE  ./maintenance_custom_analyst/${NAME}-skip.list)
@@ -30,8 +20,11 @@ while read LINE
 done < temp.txt
 rm temp.txt
 #cd maintenance_custom_analyst
-if [ NOCP == 'false' ];
-then git add --all
+if [ $NOCP == 'false' ];
+then    
+        cd maintenance_custom_analyst	
+	git add --all
 	git commit -m 'New changes in tcp-qa skip lists or checklist'
-	git push
+	git push origin master
+	cd ..
 fi
