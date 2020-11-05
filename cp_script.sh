95c3b3ea606cd5113a432f6d3c5e0b27c5d8cc8c
pglazov <pglazov@mirantis.com>
2020-11-05 13:07:09 +0000

    manual fix for run script in jenkins

--- cp_script.sh
+++ cp_script.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-cd ..
-cp tcp-qa/checklist.yaml ./maintenance_custom_analyst/
+#cd ..
+cp tcp-qa/checklist.yaml maintenance_custom_analyst/
 find . -name *skip.list | grep released-heat-cicd >>temp.txt
 NAME=''
 NOCP='true'
 while read LINE
-    do 
-	    if [[ $LINE == *"queens"* ]];
-	    then NAME=${NAME}"queens"
-	    fi
-            if [[ $LINE == *"pike"* ]];            
-            then NAME=${NAME}"pike"
-	    fi
-	    if [[ $LINE == *"contrail"* ]];
-	    then NAME=${NAME}"-contrail"
-	    else NAME=${NAME}"-dvr"
-            fi
-	    DIFF=$(diff $LINE ./maintenance_custom_analyst/${NAME}-skip.list)
+    do 	    NAME=$LINE
+	    NAME=$(echo $NAME | sed 's/.\/tcp-qa\/tcp_tests\/templates\/released-heat-cicd-//' | sed 's/\/tempest_skip.list//')
+	    #if [[ ${LINE} == *"queens"* ]];
+	    #then NAME=${NAME}"queens"
+	    #fi
+            #if [[ ${LINE} == *"pike"* ]];            
+            #then NAME=${NAME}"pike"
+	    #fi
+	    #if [[ ${LINE} == *"contrail"* ]];
+	    #then NAME=${NAME}"-contrail-sl"
+	    #else NAME=${NAME}"-dvr-sl"
+            #fi
+	    echo $NAME
+	    echo $LINE
+	    DIFF=$(diff $LINE  ./maintenance_custom_analyst/${NAME}-skip.list)
 	    if [[ "$DIFF" != "" ]]
 		 then cp $LINE ./maintenance_custom_analyst/${NAME}-skip.list
 	    		 echo 'copied '${NAME}
@@ -26,7 +29,7 @@ while read LINE
 	    NAME=''
 done < temp.txt
 rm temp.txt
-cd maintenance_custom_analyst
+#cd maintenance_custom_analyst
 if [ NOCP == 'false' ];
 then git add --all
 	git commit -m 'New changes in tcp-qa skip lists or checklist'
